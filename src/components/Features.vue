3d833984c6acd9193bc4ab8241a3fb9a6b47e4fe
ahkui <ahkui@outlook.com>
2019-10-04 20:56:51 +0800

    fix oversize

--- src/components/Features.vue
+++ src/components/Features.vue
@@ -218,7 +218,7 @@ $feature-content-spacing: 2rem;
 .feature {
   display: flex;
   padding: #{$feature-block-spacing};
-  width: calc(100% - #{$feature-block-spacing});
+  width: calc(100% - #{$feature-block-spacing} - #{$feature-block-spacing});
 
   .col {
     height: calc(
